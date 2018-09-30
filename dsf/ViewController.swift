1fb101302158cdd9fd587f7f8a2cb9a02e07d167
Your Name <you@example.com>
2018-09-30 09:44:50 +0530

    Direct push to remote

--- dsf/ViewController.swift
+++ dsf/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
         
         print("git repository added")
         
+        print("direct repository committed")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
