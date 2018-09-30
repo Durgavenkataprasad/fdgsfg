24d29b97477b19afcccb11563199afea94fa276b
Your Name <you@example.com>
2018-09-30 09:50:48 +0530

    second vc added

---
+++ dsf/NewViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NewViewController.swift
+//  dsf
+//
+//  Created by Babji Betha on 30/09/18.
+//  Copyright © 2018 Babji Betha. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("second vc added")
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
